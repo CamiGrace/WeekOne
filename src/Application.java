360fb20904f02fcd4a998fb8e70c6d9597c5a6c5
CamiG <camidougal@gmail.com>
2022-09-05 12:01:24 -0600

    Signed-off-by: CamiG <camidougal@gmail.com>

---
+++ src/Application.java
@@ -0,0 +1,30 @@
+
+public class Application {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		// create a variable to hold the quantity of available plane seats left on a flight
+				int availablePlaneSeats = 5;
+				// create a variable to hold the cost of groceries at checkout
+				double checkoutGroceriesCost = 132.64;
+				// create a variable to hold a person's middle initial
+				char middleInitial = 'G';
+				// create a variable to hold true if it's hot outside and false if it's cold outside 
+				boolean isHotOutside = true; 
+				// create a variable to hold a customer's first name
+				String customerFirstName = "Sarah";
+				// create a variable to hold a street address
+				String streetAddress = "1234 W Main";
+				//print all variables to console
+				System.out.println("Available seats:" + availablePlaneSeats);
+				System.out.println("Grocery total:" + checkoutGroceriesCost);
+				System.out.println("The person's middle initial is:" + middleInitial);
+				System.out.println("It is hot outside:" + isHotOutside);
+				System.out.println("The Customer's first name is:" + customerFirstName);
+				System.out.println("The person lives:" +  streetAddress);
+	} 
+	}
+
+
+
